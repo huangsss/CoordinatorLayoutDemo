6283c74dfd6bd4dfd19dfc76cbda7d2fa9c3e0d5
huangasys <huangays@gmail.com>
2018-06-29 17:46:31 +0800

    同步;

--- app/src/main/java/huangasys/com/materdesigndemo/MainActivity.java
+++ app/src/main/java/huangasys/com/materdesigndemo/MainActivity.java
@@ -1,7 +1,12 @@
  package huangasys.com.materdesigndemo;
 
+ import android.graphics.Color;
+ import android.os.Build;
  import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+ import android.view.View;
+ import android.view.Window;
+ import android.view.WindowManager;
 
  public class MainActivity extends AppCompatActivity {
 
@@ -11,7 +16,19 @@ import android.support.v7.app.AppCompatActivity;
         //activity_coordinator_test -- CoordinatorLayout练习;
         //activity_main -- CoordinatorLayout - Appbar - CollapsingToolbarLayout - Toolbar - FloatingActionButton联动;
         // -----具体效果运行一下就明白了-------测试
-        setContentView(R.layout.activity_main);
 
+/*        View decorView = getWindow().getDecorView();
+        int option = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN|View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+        decorView.setSystemUiVisibility(option);
+        getWindow().setStatusBarColor(Color.TRANSPARENT);*/
+     /*   if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.TRANSPARENT);
+        }*/
+        StatusBarUtil.darkMode(this);
+        setContentView(R.layout.activity_main);
     }
 }
